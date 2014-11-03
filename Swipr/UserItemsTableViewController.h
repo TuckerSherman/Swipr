cc26c270ffe2fd90986c2fe529372917774d3296
TuckerSherman <tsherman@outlook.com>
2014-11-03 11:34:26 -0800

    profile view update

---
+++ Swipr/UserItemsTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  UserItemsTableViewController.h
+//  Swipr
+//
+//  Created by Tucker Sherman on 11/3/14.
+//  Copyright (c) 2014 J and T. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UserItemsTableViewController : UITableViewController
+
+@end
