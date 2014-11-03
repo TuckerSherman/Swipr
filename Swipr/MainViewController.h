fb363fcfbe0640af3754beb48a6116f2c81cc8f3
Jacob Cho <jacobshcho@gmail.com>
2014-11-03 10:47:54 -0800

    added draggable background views

---
+++ Swipr/MainViewController.h
@@ -0,0 +1,15 @@
+//
+//  MainViewController.h
+//  Swipr
+//
+//  Created by Jacob Cho on 2014-11-03.
+//  Copyright (c) 2014 J and T. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "DraggableViewBackground.h"
+
+@interface MainViewController : UIViewController
+
+
+@end
---
+++ Swipr/MainViewController.m
@@ -0,0 +1,37 @@
+//
+//  MainViewController.m
+//  Swipr
+//
+//  Created by Jacob Cho on 2014-11-03.
+//  Copyright (c) 2014 J and T. All rights reserved.
+//
+
+#import "MainViewController.h"
+
+
+@interface MainViewController ()
+
+@end
+
+@implementation MainViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    DraggableViewBackground *draggableBackground = [[DraggableViewBackground alloc]initWithFrame:self.view.frame];
+    [self.view addSubview:draggableBackground];
+}
+
+
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
