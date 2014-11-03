c9fb7f6a75100034cb06c964a13c2a80bc813867
TuckerSherman <tsherman@outlook.com>
2014-11-03 10:20:09 -0800

    Initial Commit

---
+++ Swipr/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Swipr
+//
+//  Created by Tucker Sherman on 11/3/14.
+//  Copyright (c) 2014 J and T. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Swipr/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Swipr
+//
+//  Created by Tucker Sherman on 11/3/14.
+//  Copyright (c) 2014 J and T. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
