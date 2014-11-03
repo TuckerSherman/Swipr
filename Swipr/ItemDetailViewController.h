adbf0e000c1e5cb9bd2efbdfec3d678379ccdc0c
TuckerSherman <tsherman@outlook.com>
2014-11-03 12:05:01 -0800

    View Controllers Created

---
+++ Swipr/ItemDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  ItemDetailViewController.h
+//  Swipr
+//
+//  Created by Tucker Sherman on 11/3/14.
+//  Copyright (c) 2014 J and T. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ItemDetailViewController : UIViewController
+
+@end
