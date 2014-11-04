40605f85058569327c77a74ce32e701157f6f1f0
TuckerSherman <tsherman@outlook.com>
2014-11-03 16:50:29 -0800

    ready to merge

---
+++ Swipr/UserItems.h
@@ -0,0 +1,13 @@
+//
+//  UserItems.h
+//  Swipr
+//
+//  Created by Tucker Sherman on 11/3/14.
+//  Copyright (c) 2014 J and T. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface UserItems : NSObject
+
+@end
